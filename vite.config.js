97117bc2bd5b2ac8ffb1b9f979e8003533c00895
pm3ndoza <pmendoza2853@gmail.com>
2022-01-29 12:22:42 -0300

    url tst4

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://github.com/pm3ndoza/gha-test/',
+  base:'https://pm3ndoza.github.io/gha-test/',
   plugins: [react()]
 })

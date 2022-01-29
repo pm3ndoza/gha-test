dc8d696e2459b54784160d62fd8445222883b8dd
pm3ndoza <pmendoza2853@gmail.com>
2022-01-29 12:04:22 -0300

    Fix bug2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://pm3ndoza.github.io',
+  base:'https://pm3ndoza.github.io/',
   plugins: [react()]
 })
